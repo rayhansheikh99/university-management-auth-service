2098756503c0cb0f06f02d737b914b1810d902f6
Rayhan Sheikh <rayhan.sheikh10@gmail.com>
2023-06-13 09:34:01 +0600

    [Done] setup link-staged eslint prettier

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,18 @@
-import mongoose from "mongoose";
-import app from "./app"
-import config from "./config/index"
+import mongoose from 'mongoose'
+import app from './app'
+import config from './config/index'
 
-// database connect 
+// database connect
 async function main() {
-    try {
-        await mongoose.connect(config.database_url as string);
-        console.log('Database connected');
-        app.listen(config.port, () => {
-            console.log(`Server app listening on port ${config.port}`)
-        })
-    } catch (err) {
-        console.log('Database connection failed');
-    }
-
+  try {
+    await mongoose.connect(config.database_url as string)
+    console.log('Database connected')
+    app.listen(config.port, () => {
+      console.log(`Server app listening on port ${config.port}`)
+    })
+  } catch (err) {
+    console.log('Database connection failed')
+  }
 }
 
-main();
+main()
